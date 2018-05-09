acb42103417e0ee6bb9064290081728896ee14d7
skmt1111 <sk_1494@icloud.com>
2018-05-09 16:44:50 +0000

    modify timezone

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ module Temp
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     config.autoload_paths += %W(#{config.root}/lib)
     config.enable_dependency_loading = true
   end
